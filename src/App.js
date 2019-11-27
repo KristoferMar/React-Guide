1f1bc450ec6c01fce7d0579898a12e83c70bca23
KristoferMar <kristofer@brostu.dk>
2019-11-27 01:34:19 +0100

    h
    j

--- src/App.js
+++ src/App.js
@@ -9,13 +9,11 @@ import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
 import * as actions from './store/actions/index'
 
-
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
   render() {
-
     // This code beneith is guards
     let routes = (
       <Switch>
@@ -30,8 +28,8 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ const sideDrawer = (props) => {
     return (
         <AuxC>
             <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
